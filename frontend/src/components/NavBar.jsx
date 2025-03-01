736628d4c2e786e0ae10e6dea61ee2f646c374fe
Alaka Dahal <me@alakadahal.me>
2025-03-01 13:45:51 +0545

    update

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -6,6 +6,7 @@ function NavBar() {
         <nav className="navbar">
             <div className="navbar-brand">
                 <Link to="/">Movie App</Link>
+                <h6> by Alaka</h6>
             </div>
             <div className="navbar-links">
                 <Link to="/" className="nav-link">Home</Link>
